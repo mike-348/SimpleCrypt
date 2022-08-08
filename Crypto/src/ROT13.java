8ca3a38da1ef4d78b5bbfc22586f62203b104585
Mike <mike@Zipcoders-MacBook-Pro-41.local>
2022-08-07 21:39:18 -0400

    made cypher

--- Crypto/src/ROT13.java
+++ Crypto/src/ROT13.java
@@ -3,30 +3,53 @@ import static java.lang.Character.isUpperCase;
 import static java.lang.Character.toLowerCase;
 
 public class ROT13  {
+    private int shift;
 
     ROT13(Character cs, Character cf) {
+        this.shift = cf - cs;
     }
 
     ROT13() {
+        this.shift = 13;
     }
 
-
     public String crypt(String text) throws UnsupportedOperationException {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < text.length(); i++) {
+            char ch = text.charAt(i);
+            if((ch >= 'a' && ch <= 'm') || (ch >= 'A' && ch <= 'M')) {
+                ch += 13;
+            }
+            else if((ch >= 'n' && ch <= 'z') || (ch >= 'N' && ch <= 'Z')) {
+                ch -= 13;
+            }
+            sb.append(ch);
+        }
 
-        return "";
+        return sb.toString();
     }
 
     public String encrypt(String text) {
-        return text;
+        return crypt(text);
     }
 
     public String decrypt(String text) {
-        return text;
+        return encrypt(text);
     }
 
     public static String rotate(String s, Character c) {
+        String prefix, suffix, result;
+        if(s.charAt(0) == c) {
+            return s;
+        }
 
-        return "";
+        int index = s.indexOf(c);
+
+        prefix = s.substring(index);
+        suffix = s.substring(0, index);
+        result = prefix + suffix;
+
+        return result;
     }
 
 }
